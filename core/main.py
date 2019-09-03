e6f56c053536b4daafcfbaf1d21b6c33cea4e66b
Bruce How <brucehow4@gmail.com>
2019-09-03 14:24:58 +0800

    Add base structure and commenting in main as example

---
+++ core/main.py
@@ -0,0 +1,16 @@
+def main():
+    """
+    Google style documenting
+    
+    Arguments:
+        param1 -- param1 desc
+        param2 -- param2 desc
+    
+    Returns:
+        param3 -- param3 desc
+    """
+    pass
+
+
+if __name__ == "__main__":
+    main()
