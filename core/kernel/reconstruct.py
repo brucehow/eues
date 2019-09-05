9aef0c8c006cdaf21ab8c57394d47136f07800b1
aimonh <53869562+aimonh@users.noreply.github.com>
2019-09-06 02:00:10 +1000

    Add files via upload
    wavelet data reconstruction code

---
+++ core/kernel/reconstruct.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pywt
+import math
+from scipy import signal
+
+def proc_signal(sig, fs = 1/60, wavename = 'db5'):
+	sig = sig[~np.isnan(sig)]
+
+	b,a = signal.butter(10,fs/8,'lowpass', fs = fs)
+	sig = signal.filtfilt(b,a,sig)
+	lvl = int(math.log(24*60*60*fs/2, 2) - 2)
+	print("wavelet: {}, decomposition level: {}, fs: {}".format(wavename, lvl, fs))
+	wl = pywt.Wavelet(wavename)
+	coeff_all = pywt.wavedec(sig, wl, level=lvl)
+	recon = pywt.waverec(coeff_all, wavelet= wl)
+
+	#plt.show()
+	return recon + np.mean(sig)
+
