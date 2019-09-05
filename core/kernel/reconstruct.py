0002515c0188ad5952f13726b06401d2e1de03ac
Bruce How <brucehow4@gmail.com>
2019-09-06 01:45:38 +0800

    Fix space typo

--- core/kernel/reconstruct.py
+++ core/kernel/reconstruct.py
@@ -5,12 +5,11 @@ from scipy import signal
 
 def proc_signal(sig, fs = 1/60, wavename = 'db5'):
 	sig = sig[~np.isnan(sig)]
-
 	b,a = signal.butter(10,fs/8,'lowpass', fs = fs)
 	sig = signal.filtfilt(b,a,sig)
 	lvl = int(math.log(24*60*60*fs/2, 2) - 2)
 	print("wavelet: {}, decomposition level: {}, fs: {}".format(wavename, lvl, fs))
 	wl = pywt.Wavelet(wavename)
 	coeff_all = pywt.wavedec(sig, wl, level=lvl)
-	return (pywt.waverec(coeff_all, wavelet= wl). np.mean(sig))
+	return (pywt.waverec(coeff_all, wavelet= wl).np.mean(sig))
 
