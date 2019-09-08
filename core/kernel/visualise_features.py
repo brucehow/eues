000f00a53529b229674159227a6091cdd42adc9e
Bruce How <brucehow4@gmail.com>
2019-09-08 10:26:11 +0800

    Add figure 1 plot with given parameters

---
+++ core/kernel/visualise_features.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_features(origin_sig, proc_sig, features, casename, fs=1/60):
+    t = np.linspace(0, proc_sig.size-1, proc_sig.size)/fs/60/60
+    onsets, peaks = features
+
+    plt.figure(1)
+    plt.plot(t, origin_sig, color='red', label='Original Signal')
+    plt.plot(t, proc_sig, color='gree', label='Reconstructed Signal')
+    plt.legend()
+    plt.xlabel('Time (hours)')
+    plt.ylabel('Temperature')
+    plt.savefig(f'./output{casename}_1.png', dpi=100)
+    
