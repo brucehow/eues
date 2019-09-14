a3f7fc0dc9bbf9c3b46020cb75b1361d2b36b037
aimonh <53869562+aimonh@users.noreply.github.com>
2019-09-14 23:50:07 +0800

    added new return variable

--- core/kernel/reconstruct.py
+++ core/kernel/reconstruct.py
@@ -5,11 +5,26 @@ from scipy import signal
 
 def proc_signal(sig, fs = 1/60, wavename = 'db5'):
 	sig = sig[~np.isnan(sig)]
-	b,a = signal.butter(10,fs/8,'lowpass', fs = fs)
+
+	b,a = signal.butter(10,fs/8,'lowpass', fs=fs)
 	sig = signal.filtfilt(b,a,sig)
 	lvl = int(math.log(24*60*60*fs/2, 2) - 2)
 	print("wavelet: {}, decomposition level: {}, fs: {}".format(wavename, lvl, fs))
 	wl = pywt.Wavelet(wavename)
 	coeff_all = pywt.wavedec(sig, wl, level=lvl)
-	return (pywt.waverec(coeff_all, wavelet= wl).np.mean(sig))
+	#pdb.set_trace()
+	#cA6, cD6,cD5, cD4, cD3, cD2, cD1= coeff_all
+	coeff_all[0] = coeff_all[0]*0
+	coeff_all[-5] = coeff_all[-5]*0
+	coeff_all[-4] = coeff_all[-4]*0
+	coeff_all[-3] = coeff_all[-3]*0
+	coeff_all[-2] = coeff_all[-2]*0
+	coeff_all[-1] = coeff_all[-1]*0
+	t = np.linspace(0, sig.size-1, sig.size)/fs/60/60
+	#cA6 = cA6*0
+	#pdb.set_trace()
+	recon = pywt.waverec(coeff_all, wavelet= wl)
+
+	#plt.show()
+	return recon, np.mean(sig)
 
