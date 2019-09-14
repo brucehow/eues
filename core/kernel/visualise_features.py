1b7f352a3c42d797b69ad4b3b540218177ff2976
Bruce How <brucehow4@gmail.com>
2019-09-14 23:19:51 +0800

    Add rturns and baseline code for visualising

--- core/kernel/visualise_features.py
+++ core/kernel/visualise_features.py
@@ -1,9 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_features(origin_sig, proc_sig, features, casename, fs=1/60):
+def plot_features(origin_sig, proc_sig, base, features, casename, fs=1/60):
     t = np.linspace(0, proc_sig.size-1, proc_sig.size)/fs/60/60
-    onsets, peaks = features
+    onsets, peaks, rturns, baseline = features
+    proc_sig = proc_sig + base
 
     plt.figure(1)
     plt.plot(t, origin_sig, color='red', label='Original Signal')
@@ -15,8 +16,10 @@ def plot_features(origin_sig, proc_sig, features, casename, fs=1/60):
 
     plt.figure(2)
     plt.plot(t, proc_sig, color='red', label='Reconstructed Signal')
-    plt.scatter(onsets/fs/60/60, proc_sig[onsets], color='green', marker='o', label='Onset')
-    plt.scatter(peaks/fs/60/60,  proc_sig[peaks], color='blue', marker='*', label='Peak')
+    plt.scatter(onsets/fs/60/60, proc_sig[onsets], s=S, color='green', marker='o', label='Onset')
+    plt.scatter(peaks/fs/60/60,  proc_sig[peaks], s=S, color='blue', marker='*', label='Peak')
+    plt.scatter(rturns/fs/60/60,  proc_sig[rturns], s=S, color='black', marker='s', label='Return to baseline')
+
     plt.legend()
     plt.xlabel('Time (hours)')
     plt.ylabel('Temperature (degrees)')
