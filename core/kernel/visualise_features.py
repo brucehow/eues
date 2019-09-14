d925678c242fdfb8f9d0c619b54ecd3761dc9dd1
Bruce How <brucehow4@gmail.com>
2019-09-14 23:25:21 +0800

    Plot figure 3 without S ratio

--- core/kernel/visualise_features.py
+++ core/kernel/visualise_features.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pdb
 import matplotlib.pyplot as plt
 
 def plot_features(origin_sig, proc_sig, base, features, casename, fs=1/60):
@@ -16,11 +17,22 @@ def plot_features(origin_sig, proc_sig, base, features, casename, fs=1/60):
 
     plt.figure(2)
     plt.plot(t, proc_sig, color='red', label='Reconstructed Signal')
+    S = 100
     plt.scatter(onsets/fs/60/60, proc_sig[onsets], s=S, color='green', marker='o', label='Onset')
     plt.scatter(peaks/fs/60/60,  proc_sig[peaks], s=S, color='blue', marker='*', label='Peak')
-    plt.scatter(rturns/fs/60/60,  proc_sig[rturns], s=S, color='black', marker='s', label='Return to baseline')
-
+    plt.scatter(rturns/fs/60/60,  proc_sig[rturns], s=S, color='black', marker='s', label='Return to Baseline')
     plt.legend()
     plt.xlabel('Time (hours)')
     plt.ylabel('Temperature (degrees)')
     plt.savefig('./output/{}_2.png'.format(casename), dpi=100)
+
+    plt.figure(3)
+    plt.plot(t, proc_sig, color='red', label='Reconstructed Signal')
+    plt.scatter(onsets/fs/60/60, proc_sig[onsets], color='green', marker='o', label='onset')
+    plt.scatter(peaks/fs/60/60,  proc_sig[peaks], color='blue', marker='*', label='peak')
+    plt.legend()
+    plt.xlabel('Time (hours)')
+    plt.ylabel('Temperature (degrees)')
+    plt.xlim((t[1000], t[1000+2*24*60]))
+    plt.savefig('./output/{}_3.png'.format(casename), dpi=100)
+    plt.close('all')
