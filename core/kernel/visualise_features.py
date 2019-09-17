7cff1521977167aa5a8e2751bbc5955e945ba394
KristianTricoli <22231727@student.uwa.edu.au>
2019-09-17 14:06:15 +0800

    Add code to plot spectogram

--- core/kernel/visualise_features.py
+++ core/kernel/visualise_features.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pdb
 import matplotlib.pyplot as plt
+import pywt as wavelet
 
 def plot_features(origin_sig, proc_sig, base, features, casename, fs=1/60):
     t = np.linspace(0, proc_sig.size-1, proc_sig.size)/fs/60/60
@@ -36,3 +37,38 @@ def plot_features(origin_sig, proc_sig, base, features, casename, fs=1/60):
     plt.xlim((t[1000], t[1000+2*24*60]))
     plt.savefig('./output/{}_3.png'.format(casename), dpi=100)
     plt.close('all')
+	
+def plot_spectrum(sig, wavename, casename, fs=1/60):
+	dat = sig
+	dt = 1/fs
+	t = np.linspace(0, sig.size-1, sig.size)/fs/60/60
+	#N = dat.size
+	#t = numpy.arange(0, N) * dt + t0
+	#p = numpy.polyfit(t - t0, dat, 1)
+	#dat_notrend = dat - numpy.polyval(p, t - t0)
+	#std = dat_notrend.std()  # Standard deviation
+	#var = std ** 2  # Variance
+	dat_norm = dat / dat.std()  # Normalized dataset
+	mother = wavelet.Morlet(6)
+	s0 = 2 * dt  
+	dj = 1 / 12  
+	J = 7 / dj
+	wave, scales, freqs, coi, fft, fftfreqs = wavelet.cwt(dat_norm, dt, dj, s0, J, mother)
+	power = (np.abs(wave)) ** 2
+	period = 1 / freqs/60/60
+	#pdb.set_trace()
+	figprops = dict(figsize=(11, 8), dpi=72)
+	fig = plt.figure(**figprops)
+	ax = plt.axes([0.1, 0.75, 0.65, 0.2])
+	plt.plot(t, sig)
+	bx = plt.axes([0.1, 0.37, 0.65, 0.28], sharex=ax)
+	levels = [0.0625, 0.125, 0.25, 0.5, 1, 2, 4, 8, 16]
+	bx.contourf(t, period, power, np.log2(levels),
+			extend='both', cmap=plt.cm.prism)
+	#bx.set_title('b) {} Wavelet Power Spectrum ({})'.format(label, mother.name))
+	bx.set_ylabel('Period (hours)')
+	bx.set_xlabel('Time (hours)')
+	plt.savefig('./output/{}_4.png'.format(casename), dpi = 100)
+	plt.close('all')
+	#plt.show()
+
