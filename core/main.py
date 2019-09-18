faaaa2dd52e9267448fcc9fee6d2e6f4685ce000
Bruce How <brucehow4@gmail.com>
2019-09-18 20:59:42 +0800

    Function calls to package rather than file

--- core/main.py
+++ core/main.py
@@ -8,15 +8,15 @@ def load_data(input_file):
 
 def main():
 	inputfile = './input/Tb data Cheetah.xlsx'
-	data = kernels.load_data(inputfile)
+	data = load_data(inputfile)
 	cols  = data.columns.values
 
 	for i in range(2, len(cols)):
 		print("start to process test case: {}".format(cols[i]))
 		orig_sig  = np.array(data.iloc[:,i])
-		proc_sig, base  = kernels.proc_signal(orig_sig)
-		features  = kernels.get_features(proc_sig + base)
-		kernels.plot_features(orig_sig, proc_sig, base, features, cols[i])
+		proc_sig, base  = kernel.proc_signal(orig_sig)
+		features  = kernel.get_features(proc_sig + base)
+		kernel.plot_features(orig_sig, proc_sig, base, features, cols[i])
 		print("end of test case: {}\n".format(cols[i]))
 if __name__ == '__main__':
 	main()
