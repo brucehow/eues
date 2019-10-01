6ddb6dc426a56abef01abd029abf2eeb7d4b790d
Ramsey Fu <3325859058@qq.com>
2019-10-01 19:27:04 +0800

    Modify code to detect peak and R

--- core/kernel/get_features.py
+++ core/kernel/get_features.py
@@ -58,3 +58,20 @@ def get_onsets(baseline, mask_len):
             start,end = -1,-1
     #pdb.set_trace()
     return np.array(onsets)
+
+def get_peaks(sig, onsets):
+    peaks, areas = [], []
+    for i in range(len(onsets)-1):
+        peak = onsets[i] + np.argmax(sig[onsets[i]:onsets[i+1]])
+        peaks.append(peak)
+        areas.append(np.sum(sig[onsets[i]:onsets[i+1]]))
+    return np.array(peaks), np.array(areas)
+
+def get_returns(sig, onsets, peaks, sd):
+    rturns = np.zeros_like(peaks)
+    for i in range(len(peaks)):
+        sample = np.abs(sig[peaks[i]:onsets[i+1]] - sd - sig[onsets[i+1]])
+        rturns[i] = peaks[i] + np.argmin(sample)
+    return rturns
+def get_baseline(sig, struct_len):
+    return get_onsetsline(sig, struct_len)
