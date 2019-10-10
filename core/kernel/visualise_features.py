62501fa477cc9c8205ecd031893922fb9e797d06
KristianTricoli <22231727@student.uwa.edu.au>
2019-10-10 15:54:45 +0800

    Clean up visualise_features

--- core/kernel/visualise_features.py
+++ core/kernel/visualise_features.py
@@ -1,49 +1,59 @@
 import numpy as np
-import pdb
+import pywt
 import matplotlib.pyplot as plt
-import pywt as wavelet
+import pdb
 
 def plot_features(origin_sig, proc_sig, base, features, casename, fs=1/60):
-    t = np.linspace(0, proc_sig.size-1, proc_sig.size)/fs/60/60
-    onsets, peaks, rturns, baseline = features
-    proc_sig = proc_sig + base
-
-    plt.figure(1)
-    plt.plot(t, origin_sig, color='red', label='Original Signal')
-    plt.plot(t, proc_sig, color='green', label='Reconstructed Signal')
-    plt.legend()
-    plt.xlabel('Time (hours)')
-    plt.ylabel('Temperature (degrees)')
-    plt.savefig('./output{}_1.png'.format(casename), dpi=100)
-
-    plt.figure(2)
-    plt.plot(t, proc_sig, color='red', label='Reconstructed Signal')
-    S = 100
-    plt.scatter(onsets/fs/60/60, proc_sig[onsets], s=S, color='green', marker='o', label='Onset')
-    plt.scatter(peaks/fs/60/60, proc_sig[peaks], s=S, color='blue', marker='*', label='Peak')
-    plt.scatter(rturns/fs/60/60, proc_sig[rturns], s=S, color='black', marker='s', label='Return to Baseline')
-    plt.legend()
-    plt.xlabel('Time (hours)')
-    plt.ylabel('Temperature (degrees)')
-    plt.savefig('./output/{}_2.png'.format(casename), dpi=100)
-
-    plt.figure(3)
-    plt.plot(t, proc_sig, color='red', label='Reconstructed Signal')
-    plt.scatter(onsets/fs/60/60, proc_sig[onsets], color='green', marker='o', label='Onset')
-    plt.scatter(peaks/fs/60/60, proc_sig[peaks], color='blue', marker='*', label='Peak')
-    plt.legend()
-    plt.xlabel('Time (hours)')
-    plt.ylabel('Temperature (degrees)')
-    plt.xlim((t[1000], t[1000+2*24*60]))
-    plt.savefig('./output/{}_3.png'.format(casename), dpi=100)
-    plt.close('all')
+	t = np.linspace(0, proc_sig.size-1, proc_sig.size)/fs/60/60
+	onsets, peaks, rturns, baseline = features
+	proc_sig = proc_sig + base
+	plot_spectrum(origin_sig[0:-1:10], 'cgau8', casename, fs=fs/10)
 	
-def plot_spectrum(sig, wavename, casename, fs=1/60):
+	plt.figure(2)
+	plt.plot(t, origin_sig, color = 'red', label='original signal')
+	plt.plot(t, proc_sig , color = 'green', label = 'reconstruct signal')
+	plt.legend()
+	plt.xlabel('Time(hours)')
+	plt.ylabel('Temperature')
+	plt.savefig('./output/{}_1.png'.format(casename), dpi=100)
+	plt.show()
+	plt.close('all')
+
+	plt.figure(3)
+	plt.plot(t, proc_sig, color = 'red', label = 'reconstruct signal')
+
+	S = 100
+	plt.scatter(onsets/fs/60/60, proc_sig[onsets], s= S, color = 'green', marker = 'o', label = 'onset')
+	plt.scatter(peaks/fs/60/60,  proc_sig[peaks], s= S, color = 'blue', marker = '*', label = 'peak')
+	plt.scatter(rturns/fs/60/60,  proc_sig[rturns], s=S, color = 'black', marker = 's', label = 'return to baseline')
+	plt.legend()
+	plt.xlabel('Time(hours)')
+	plt.ylabel('Temperature')
+	plt.savefig('./output/{}_2.png'.format(casename), dpi=100)
+	plt.show()
+	plt.close('all')
+
+
+	plt.figure(4)
+	plt.plot(t, proc_sig, color = 'red', label = 'reconstruct signal')
+	plt.scatter(onsets/fs/60/60, proc_sig[onsets], color = 'green', marker = 'o', label = 'onset')
+	plt.scatter(peaks/fs/60/60,  proc_sig[peaks], color = 'blue', marker = '*', label = 'peak')
+	plt.legend()
+	plt.xlabel('Time(hours)')
+	plt.ylabel('Temperature')
+	plt.xlim((t[1000], t[1000+2*24*60]))
+	plt.savefig('./output/{}_3.png'.format(casename), dpi=100)
+	plt.show()
+	plt.close('all')
+
+
+def plot_spectrum(sig, wavename, casename, fs = 1/60):
+	import pycwt as wavelet
 	dat = sig
 	dt = 1/fs
 	t = np.linspace(0, sig.size-1, sig.size)/fs/60/60
 
-	dat_norm = dat / dat.std()  # Normalized dataset
+	dat_norm = dat / dat.std()
 	mother = wavelet.Morlet(6)
 	s0 = 2 * dt  
 	dj = 1 / 12  
@@ -58,8 +68,16 @@ def plot_spectrum(sig, wavename, casename, fs=1/60):
 	bx = plt.axes([0.1, 0.37, 0.65, 0.28], sharex=ax)
 	levels = [0.0625, 0.125, 0.25, 0.5, 1, 2, 4, 8, 16]
 	bx.contourf(t, period, power, np.log2(levels),
-			extend='both', cmap=plt.cm.prism)
+            extend='both', cmap=plt.cm.prism)
 	bx.set_ylabel('Period (hours)')
 	bx.set_xlabel('Time (hours)')
 	plt.savefig('./output/{}_4.png'.format(casename), dpi = 100)
+	plt.show()
 	plt.close('all')
+
+
+
+
+
+
+
