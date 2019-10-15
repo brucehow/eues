f6d4042966c8698c402e3e845521bed514c3cec8
JXQuinlivan <jeremy.quinlivan@gmail.com>
2019-10-15 16:12:34 +0800

    Add code for the first generate button
    Project won't be in a working state till GUI developed further

--- core/main.py
+++ core/main.py
@@ -4,9 +4,8 @@ import shutil
 import tkinter.font as tkFont
 import os
 import analyse
-from tkinter import ttk
 from PIL import Image, ImageTk 
-from tkinter.filedialog import askopenfilename,askdirectory
+from tkinter.filedialog import askopenfilename
 from tkinter.messagebox import showerror
 
 def gui():
@@ -22,6 +21,31 @@ def gui():
 	s5,s6,s7,s8 = tk.StringVar(),tk.StringVar(),tk.StringVar(),tk.StringVar()
 	s9,s10,s11,s12 = tk.StringVar(),tk.StringVar(),tk.StringVar(),tk.StringVar()
 	s13,s14  = tk.StringVar(),tk.StringVar()
+	
+	def selectPath():
+		path.set(askopenfilename())
+
+	def button1_func(infile, testcase_name, sample_interval,  th_begin, th_end, wavelet, thd):
+		for file in os.listdir('./output'):
+			os.remove('./output/' + file)
+		err, ret = analyse.start_analysis(infile, th_begin, th_end, wavelet, sample_interval, testcase_name, thd)
+		if True != err:
+			showerror(title="Error", message= "{}".format(err))
+			
+		s1.set('{:.3g}'.format(ret[0]))
+		s2.set('{:.3g}'.format(ret[1]))
+		s3.set('{:.3g}'.format(ret[2]))
+		s4.set('{:.3g}'.format(ret[3]))
+		s5.set('{:.3g}'.format(ret[4]))
+		s6.set('{:.3g}'.format(ret[5]))
+		s7.set('{:.3g}'.format(ret[6]))
+		s8.set('{:.3g}'.format(ret[7]))
+		s9.set('{:.3g}'.format(ret[8]))
+		s10.set('{:.3g}'.format(ret[9]))
+		s11.set('{:.3g}'.format(ret[10]))
+		s12.set('{:.3g}'.format(ret[11]))
+		s13.set('{:.3g}'.format(ret[12]))
+		s14.set('{:.3g}'.format(ret[13]))
 
 if __name__ == '__main__':
 	main()
