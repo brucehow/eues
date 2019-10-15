ee8fb7971339272ba212528afa25e91b4257392f
KristianTricoli <53737432+KristianTricoli@users.noreply.github.com>
2019-10-15 15:33:23 +0800

    Write function to load data and set up structure

--- core/analyse.py
+++ core/analyse.py
@@ -1,1 +1,18 @@
-# Analysis code here
+import kernel
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def load_data(input_file, th_begin, th_end, wavelet, sample_interval, testcase_name):
+	try:
+		data = pd.read_excel(input_file)
+	except Exception:
+		return 'fail to open {}'.format(input_file), []
+	else:
+		return True,data
+  
+def check_input(data, th_begin, th_end, sample_interval, testcase_name, thd):
+  pass
+
+def start_analysis(infile, th_begin, th_end, wavelet, sample_interval, testcase_name, thd):
+  pass
