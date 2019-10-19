eb8d4ebebe37cdaca7ac51c4b666fcae997ab222
KristianTricoli <53737432+KristianTricoli@users.noreply.github.com>
2019-10-19 13:39:56 +0800

    Clean and simplify reconstruct.py

--- core/kernel/reconstruct.py
+++ core/kernel/reconstruct.py
@@ -2,25 +2,22 @@ import numpy as np
 import pywt
 import math
 from scipy import signal
+import pdb
 
 def proc_signal(sig, fs=1/60, wavename='db5'):
 	sig = sig[~np.isnan(sig)]
 
-	b,a = signal.butter(10,fs/8,'lowpass', fs=fs)
+	b,a = signal.butter(10,fs/3,'lowpass', fs=fs)
 	sig = signal.filtfilt(b,a,sig)
-	lvl = int(math.log(24*60*60*fs/2, 2) - 2)
+	lvl = int(math.log(24*60*60*fs/2, 2)-2)
 	print("wavelet: {}, decomposition level: {}, fs: {}".format(wavename, lvl, fs))
 	wl = pywt.Wavelet(wavename)
 	coeff_all = pywt.wavedec(sig, wl, level=lvl)
 
-	coeff_all[0] = coeff_all[0]*0
-	coeff_all[-5] = coeff_all[-5]*2
-	coeff_all[-4] = coeff_all[-4]*2
-	coeff_all[-3] = coeff_all[-3]*2
-	coeff_all[-2] = coeff_all[-2]*2
-	coeff_all[-1] = coeff_all[-1]*2
+	for idx, coeff in enumerate(coeff_all):
+		if 0 == idx or idx >= 2 :
+			coeff_all[idx] = coeff*0
 	t = np.linspace(0, sig.size-1, sig.size)/fs/60/60
 	recon = pywt.waverec(coeff_all, wavelet= wl)
-
 	return recon, np.mean(sig)
 
